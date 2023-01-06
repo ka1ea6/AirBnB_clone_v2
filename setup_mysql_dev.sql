a0a9211c40d028d71496b49cfe234165440da868
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-06 19:32:20 +0300

    mysql setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,9 @@
+-- SQL script to prepare a mysql server for the project
+-- Creates a database if it does not exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Creates a user for using the database
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grants all privilages to the user 'hbnb_dev' on the database 'hbnb_dev_db'
 GRANT ALL PRIVILEGES  ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grants select privilages to 'hbnb_dev'
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
