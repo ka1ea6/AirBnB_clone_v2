2c207e7785e6ff9570ad01b902bfd42b30a8c5fe
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-23 11:52:52 +0300

    some changes on 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,16 +5,17 @@ the contents of the web_static folder.'''
 
 from fabric.api import local
 from datetime import datetime
-from os import path
+import os.path
 
 
 def do_pack():
     '''Function to archive the web_static file'''
     dt = datetime.utcnow()
 
-    file_name = f"versions/web_static_{dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}.tgz"
+    file_name = f"versions/web_static\
+    {dt.year}{dt.month}{dt.day}{dt.hour}{dt.minute}{dt.second}.tgz"
 
-    if path.isdir("versions") is False:
+    if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
     if local(f"tar -cfvz {file_name} web_static").failed is True:
