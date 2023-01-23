66f1cd475b1f8528c239bc338db318ebce316a4f
ka1ea6 <kaleab77gulilat@gmail.com>
2023-01-23 11:54:12 +0300

    req changes on 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''Fabric script to generate a .tgz archive from
-the contents of the web_static folder.'''
+# Fabfiel to generate a tar archive from the contents of
+# web_static
 
 
 from fabric.api import local
