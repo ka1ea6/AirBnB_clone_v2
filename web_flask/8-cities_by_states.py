b6175ce91e828b44ceb15f3c81cf96edffa38d0d
ka1ea6 <kaleab77gulilat@gmail.com>
2023-02-07 11:16:48 +0300

    8 PEP code

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,6 +13,7 @@ storage_type = getenv("HBNB_TYPE_STORAGE")
 
 app = Flask(__name__)
 
+
 @app.route("/cities_by_states", strict_slashes=False)
 def states_list():
     '''Method to render an HTML page'''
@@ -20,10 +21,12 @@ def states_list():
 
     return render_template("8-cities_by_states.html", states=states)
 
+
 @app.teardown_appcontext
 def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run("0.0.0.0")
