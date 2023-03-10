9085ef2172fd2bb8f1b8fc2c363a378ef47cc73e
ka1ea6 <kaleab77gulilat@gmail.com>
2023-03-10 06:20:30 +0000

    hello route change

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,10 +10,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage", strict_slashes=False)
 def index():
     '''Displays Hello, HBNB for requests to /'''
-    return 'Hello, HBNB!'
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
